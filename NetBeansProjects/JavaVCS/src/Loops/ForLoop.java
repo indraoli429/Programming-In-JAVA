53502fde672b709418505a8c886114db0f46cb00
khillsman3 <indraoli429@gmail.com>
2019-02-25 22:46:13 +0545

    pattern display in java

--- NetBeansProjects/JavaVCS/src/Loops/ForLoop.java
+++ NetBeansProjects/JavaVCS/src/Loops/ForLoop.java
@@ -10,5 +10,25 @@ package Loops;
  * @author indra
  */
 public class ForLoop {
+     public static void main(String[]args)
+    {
+        for(int i=6;i>=0;i--) 
+        {   
+            for(int j=1;j<i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+        for(int i=0;i<6;i++) 
+        {   
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
     
 }
