7c4b10b8a924b2339efcc5b64e8a098cd9bd0997
Indra Bahadur Oli <indraoli429@gmail.com>
2019-08-20 00:52:12 +0545

    Project Name changed

---
+++ NetBeansProjects/CoreJavaExample/src/abstractAndInterface/Animal.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstractAndInterface;
+
+/**
+ *
+ * @author indra
+ */
+public abstract class Animal
+{
+  public abstract void eat();
+  public abstract void sleep();
+}
---
+++ NetBeansProjects/CoreJavaExample/src/abstractAndInterface/Car.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstractAndInterface;
+
+/**
+ *
+ * @author indra
+ */
+public abstract class Car
+{
+  public String name;
+    public String description;
+    public abstract void display(); 
+}
